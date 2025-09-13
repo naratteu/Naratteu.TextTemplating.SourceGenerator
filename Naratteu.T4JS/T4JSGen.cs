878f3c33bdd2571756c0f60949846d0530ff33a0
naratteu <skfhddlg@naver.com>
2025-09-13 14:35:11 +0900

    T4JS

--- Naratteu.T4JS/T4JSGen.cs
+++ Naratteu.T4JS/T4JSGen.cs
@@ -1,24 +1,29 @@
-using Mono.TextTemplating;
+using System.Text;
 using Microsoft.CodeAnalysis;
+using NiL.JS.Core;
 
 [Generator]
-public class TTGen : IIncrementalGenerator
+public class T4JSGen : IIncrementalGenerator
 {
-	void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext ctx)
-	{
-		var ss = ctx.AdditionalTextsProvider.Where(f => Path.GetExtension(f.Path) is ".t4" or ".tt").Select((file, c) =>
-		{
-			ctx.RegisterPostInitializationOutput((ctx) =>
-			{
-				var te = new TemplatingEngine();
-				te.UseInProcessCompiler(); // 호출하지 않았을때와의 차이를 모르겠습니다. 의미가 있나요?
-				ctx.AddSource($"TT{DateTime.Now.Ticks}.g.cs", // todo: 적절한 이름을 생성해야합니다. 
-				te.ProcessTemplateAsync(file.GetText()?.ToString(), new TemplateGenerator()).Result);
-			});
-			return file;
-		});
-		// 이렇게 탐색된 파일들에 대해 PostInitialization으로 등록해야 VisualStudio 상에서 생성된 소스에 접근이 가능했음.
-		// 허나 단순 RegisterSourceOutput 으로는 안되는 이유는 알수가 없음. 버그인지?
-		ctx.RegisterSourceOutput(ss, (a, b) => { });
-	}
+    void IIncrementalGenerator.Initialize(IncrementalGeneratorInitializationContext ctx)
+    {
+        var ss = ctx.AdditionalTextsProvider
+            .Where(file => file.Path.EndsWith(".t4.js"))
+            .Select((file, c) =>
+            {
+                ctx.RegisterPostInitializationOutput((ctx) =>
+                {
+                    var te = new Context();
+                    var sb = new StringBuilder($"// Gen From {file.Path}\n");
+                    te.DefineVariable("echo").Assign(te.GlobalContext.ProxyValue(new Action<string>(echo => sb.AppendLine(echo)))); //todo: console.log로 하고싶음
+                    te.Eval(file.GetText()?.ToString());
+                    ctx.AddSource($"TT{DateTime.Now.Ticks}.g.cs", // todo: 적절한 이름을 생성해야합니다.
+                    sb.ToString());
+                });
+                return file;
+            });
+        // 이렇게 탐색된 파일들에 대해 PostInitialization으로 등록해야 VisualStudio 상에서 생성된 소스에 접근이 가능했음.
+        // 허나 단순 RegisterSourceOutput 으로는 안되는 이유는 알수가 없음. 버그인지?
+        ctx.RegisterSourceOutput(ss, (a, b) => { });
+    }
 }
--- README.md
+++ README.md
@@ -1,21 +1,14 @@
-# Naratteu.TextTemplating.SourceGenerator
+# Naratteu.T4JS
 
 ## Play
 
 ```bash
-git clone https://github.com/naratteu/Naratteu.TextTemplating.SourceGenerator
-cd Naratteu.TextTemplating.SourceGenerator
-cd Naratteu.TextTemplating.SourceGenerator
+git clone https://github.com/naratteu/Naratteu.T4JS
+cd Naratteu.T4JS
+cd Naratteu.T4JS
 dotnet pack -p:Version=0.0.1-test
 dotnet run --project ../Playground
 # Hello, 1764!
-```
-
-## WTF
-
-```bash
-docker run -it --rm -v ..\:/app mcr.microsoft.com/dotnet/sdk:8.0 dotnet run --project app/Playground
-# err
 ```
 
 ## See Also
@@ -23,3 +16,5 @@ docker run -it --rm -v ..\:/app mcr.microsoft.com/dotnet/sdk:8.0 dotnet run --pr
 - https://github.com/CptWesley/T4.SourceGenerator
 - https://github.com/JakeSays/TextTemplating
 - https://github.com/jgiannuzzi/T4.Build
+- https://github.com/ufcpp/ScribanSourceGenerator
+- https://github.com/peachpiecompiler
